1f95a9cf42c76620e0f25b6608994c3c960fd85f
dercko4 <dercko4@mail.ru>
2024-04-27 18:30:43 +0300

    123

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ module.exports=new Sequelize(
     process.env.DB_PASSWORD,
     {
         dialect:'postgres',
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST || "localhost",
         pool: {
             max: 100,
             min: 0,
