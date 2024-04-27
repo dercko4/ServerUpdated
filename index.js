bee3941c571cc170b41e7761b2c19be10e7662dd
dercko4 <dercko4@mail.ru>
2024-04-27 19:08:56 +0300

    123

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {User, UserStorage} = require('./models/model')
 
 const http = require('http')
 const HOST=process.env.HOST
-const PORT = 'localhost'
+const PORT = 6800
 const app = express()
 const server = http.createServer(app)
 
